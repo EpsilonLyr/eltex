ff1f63d514d1c921afff1108df813a765e6bed52
EpsilonLyr <79781751+EpsilonLyr@users.noreply.github.com>
2021-10-17 17:33:56 +0700

    Update 2.sh

--- eltex/labs/lab3/2.sh
+++ eltex/labs/lab3/2.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+#Создать full.log, в который вывести строки файла /var/log/rhsm/rhsm.log, содержащие предупреждения и информационные сообщения,
+#заменив маркеры предупреждений и информационных сообщений на слова Warning: и Information:,
+#чтобы в получившемся файле сначала шли все информационные сообщения, а потом все предупреждения. Вывести этот файл на экран.
 grep -a -E  "\\(WW\\)" /var/log/rhsm/rhsm.log | sed -E "s/$war/Warning: /" > full.log
 grep -a -E  "\\(II\\)" /var/log/rhsm/rhsm.log | sed -E "s/$inf/Information: /" >> full.log
