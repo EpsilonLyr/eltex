b9b70dad3e323061c8565ef5fcf367ae3004fc28
EpsilonLyr <epsilonlyrae@yahoo.com>
2021-10-17 16:58:31 +0700

    commit project

---
+++ eltex/labs/lab4/menu.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+answer=0
+param="a"
+
+while ! [[ $answer -eq 3 ]]
+do
+
+echo "введите 1, чтобы сделать бэкап файла"
+echo "введите 2, чтобы восстановить файл"
+echo "введите 3, чтобы чтобы выйти"
+read answer 
+case $answer in
+1)
+echo "введите имя файла"
+read param
+exec ./remftrash $param  
+;;
+2)
+echo "введите имя файла"
+read param
+exec ./unftrash $param  
+;;
+esac
+done
