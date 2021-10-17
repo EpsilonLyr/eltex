14762999edd1acbb3af8ad1868c629cd824a20bc
EpsilonLyr <epsilonlyrae@yahoo.com>
2021-10-17 18:09:58 +0700

    commit

--- eltex/labs/lab4/menu.sh
+++ eltex/labs/lab4/menu.sh
@@ -2,12 +2,13 @@
 answer=0
 param="a"
 
-while ! [[ $answer -eq 3 ]]
+while ! [[ $answer -eq 4 ]]
 do
 
 echo "введите 1, чтобы сделать бэкап файла"
 echo "введите 2, чтобы восстановить файл"
-echo "введите 3, чтобы чтобы выйти"
+echo "введите 3, чтобы посмотреть, какие файлы можно восстановить"
+echo "введите 4, чтобы чтобы выйти"
 read answer 
 case $answer in
 1)
@@ -19,6 +20,9 @@ case $answer in
   echo "введите имя файла"
   read param
   exec ./unftrash $param  
+;;
+3)
+ exec ./availebleFilesForBackup
 ;;
 esac
 done
